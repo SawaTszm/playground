79fcb1b7fcebf4368c2060653e5f232caf0c32bf
sawaTszm <sawa.tsuzumi@gmail.com>
2023-02-11 14:12:40 +0900

    🚧 remove xIsNext from state

--- tutorial-react/react-example/src/App.tsx
+++ tutorial-react/react-example/src/App.tsx
@@ -78,21 +78,19 @@ function calculateWinner(squares: string[]) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState<boolean>(true);
   const [history, setHistory] = useState<string[][]>([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  const currentSquares = history[currentMove];
 
+  const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
   function handlePlay(nextSquares: string[]) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove: number) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
