87bcc2206dea06296e149640ae522fb9a356d84a
sawaTszm <sawa.tsuzumi@gmail.com>
2023-03-04 13:19:33 +0900

    🚧 flutter-codelab-first: Male the app prettier

--- tutorial-flutter/tutorialapp/lib/main.dart
+++ tutorial-flutter/tutorialapp/lib/main.dart
@@ -17,7 +17,8 @@ class MyApp extends StatelessWidget {
           title: "Namer App",
           theme: ThemeData(
               useMaterial3: true,
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange)),
+              colorScheme:
+                  ColorScheme.fromSeed(seedColor: Colors.deepPurpleAccent)),
           home: MyHomePage(),
         ));
   }
@@ -46,20 +47,23 @@ class MyHomePage extends StatelessWidget {
       // ColumnはFlutterで最も基本的なレイアウトウィジェットの1つです。
       // 任意の数の子ウィジェットを受け取り、上から下へ列を作ることができます。
       // デフォルトでは、カラムは視覚的に子を一番上に配置します。すぐにこれを変更して、カラムが中央に配置されるようにします。
-      body: Column(
-        children: [
-          Text("A random AWESOME idea:"),
-          // この2番目のテキスト・ウィジェットは、appStateを受け取り、そのクラスの唯一のメンバーであるcurrent（これはWordPairです）にアクセスします。
-          // WordPairは、asPascalCaseやasSnakeCaseなど、便利なゲッターをいくつか提供しています。ここではasLowerCaseを使用していますが、他の選択肢を好むのであれば、今すぐ変更できます。
-          BigCard(pair: pair),
-          ElevatedButton(
-              onPressed: () {
-                appState.getNext();
-              },
-              child: Text("次へ"))
-          // Flutterのコードがいかに末尾のカンマを多用しているかに注目してください。なぜならchildrenはこの特定のColumnパラメータリストの最後の（そして唯一の）メンバーだからです。
-          // しかし、一般的に末尾のカンマを使うのは良い考えです。メンバーを増やすのが簡単になりますし、Dartの自動整形がそこに改行を入れるようにするヒントにもなります。詳細は、「コードの書式設定」を参照してください。
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // この2番目のテキスト・ウィジェットは、appStateを受け取り、そのクラスの唯一のメンバーであるcurrent（これはWordPairです）にアクセスします。
+            // WordPairは、asPascalCaseやasSnakeCaseなど、便利なゲッターをいくつか提供しています。ここではasLowerCaseを使用していますが、他の選択肢を好むのであれば、今すぐ変更できます。
+            BigCard(pair: pair),
+            SizedBox(height: 10), // 大体隙間(margin)を取るために使われるWidget
+            ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text("次へ"))
+            // Flutterのコードがいかに末尾のカンマを多用しているかに注目してください。なぜならchildrenはこの特定のColumnパラメータリストの最後の（そして唯一の）メンバーだからです。
+            // しかし、一般的に末尾のカンマを使うのは良い考えです。メンバーを増やすのが簡単になりますし、Dartの自動整形がそこに改行を入れるようにするヒントにもなります。詳細は、「コードの書式設定」を参照してください。
+          ],
+        ),
       ),
     );
   }
@@ -77,13 +81,31 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // 現在のテーマをthemeに保存
+    var theme = Theme.of(context);
+    // theme.textThemeにアクセスすることでアプリのフォントテーマにアクセスできる
+    // nullを許容してる変数(TextStyle?: displayMedium)をCasting away nullabilityするための"bank operator"が"!"
+    // (displayMedium as TextStyle)と同義。
+    // copyWith: 定義した変更を加えたTextStyleを返す。
+    // ⌘⇧spaceで、関数の引数として渡せる値の完全なリストが見られる(!)
+    var style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
     // 注：Flutterでは、できる限り継承ではなく、合成を使います。ここでは、paddingがTextの属性である代わりに、ウィジェットになっているのです。
     // こうすることで、ウィジェットはその1つの責任に集中でき、開発者はUIをどのように構成するか、完全に自由にできるようになります。
     // 例えば、Paddingウィジェットを使って、テキスト、画像、ボタン、独自のカスタムウィジェット、またはアプリ全体をパッド化することができます。ウィジェットは、何を包んでいるかは気にしません。
     return Card(
+      // 現在のテーマのcolorSchemeに沿って色を決定する(primary: メインカラー)
+      color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asLowerCase),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          // スクリーンリーダー用とかに読み上げやすいPascalCaseにする
+          semanticsLabel: pair.asPascalCase,
+        ),
       ),
     );
   }
